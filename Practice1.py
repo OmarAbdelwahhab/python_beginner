ba15a81095e09b4844e97a48da7a2475d918db92
XplodingMoose <omar.abdelwahhab@gmail.com>
2020-03-28 18:08:22 -0600

    initial commit

---
+++ Practice1.py
@@ -0,0 +1,10 @@
+print("Hello Everyone!")
+a=2
+b=2
+name="David"
+surname="Hayward"
+print(name, surname)
+print(a+b)
+
+
+
---
+++ Practice3.py
@@ -0,0 +1,10 @@
+print("Halt, who goes there?")
+name=input()
+if name=="David":
+    print("Welcome, good sir. You may pass")
+else:
+    print("I know you not. Prepare for battle")
+    
+
+
+
